779eba974c08e79af285553d05e12405d5879900
01 <ahmedmagdy92@yahoo.com>
2019-06-10 09:11:55 +0200

    added Pillow

--- trial/settings.py
+++ trial/settings.py
@@ -142,5 +142,5 @@ MEDIA_URL = "/media/"
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ['EMAIL'] #set on server
-EMAIL_HOST_PASSWORD = os.environ['PASSWORD'] #set on server
+#EMAIL_HOST_USER = os.environ['EMAIL'] #set on server
+#EMAIL_HOST_PASSWORD = os.environ['PASSWORD'] #set on server
